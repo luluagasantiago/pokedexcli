7b48365204212b42c0242aebc2ffc3e69d6cfcd5
luluagasantiago <luluagasantiago@icloud.com>
2024-01-22 22:38:30 +0100

    added a client to interacrt with the poke api

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
+import (
+	"fmt"
+	"log"
+
+	"github.com/luluagasantiago/pokedexcli/internal/pokeapi"
+)
+
 //build a REPL that reads a pokemon name and prints its stats
 
 func main() {
-	startRepl()
+	pokeapiClient := pokeapi.NewClient()
+
+	resp, err := pokeapiClient.ListLocationAreas()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+	fmt.Println(resp)
+
+	//startRepl()
 
 }
